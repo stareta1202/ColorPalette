555a27e604c45a2b296a900383f9e9f78965f7e3
yongjun <stareta1202@gmail.com>
2021-12-24 13:15:42 +0900

    Initial Commit

---
+++ Shared/PaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteApp.swift
+//  Shared
+//
+//  Created by yjlee12 on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
